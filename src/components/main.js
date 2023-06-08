bf8a036a787bd1b6985043b81b800429e009eeab
Sbsbsv <sbsbsv002@gmail.com>
2023-06-09 00:30:38 +0530

    Intital commit

---
+++ src/components/main.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export const Main = () => {
+  return (
+    <>
+    <div className="outer">
+<div className="inner">
+        <div className="firstdiv">
+            <div className="left">
+                <div className="circle">
+                    <p>19191919</p>
+                </div>
+                <div className="table">
+                <table >
+        <tr>
+            <th>Name</th>
+            <td>nandu</td>
+            
+        </tr>
+        <tr>
+            <th>Gender</th>
+            <td>Male</td>
+            
+        </tr>
+        <tr>
+            <th>Email address</th>
+            <td>something</td>
+            
+        </tr>
+        <tr>
+            <th>Contact No.</th>
+            <td>1233211234</td>
+          
+        </tr>
+        <tr>
+            <th>Address</th>
+            <td>Basant vihar,Street No 5, H.No.-S665,New DelehiBasant vihar,Street No 5, H.No.-S665,New Delehi</td>
+          
+        </tr>
+    </table>
+                </div>
+            </div>
+            <div className="right">
+                <div className="photodiv">
+                    <img className='simage' src="https://source.unsplash.com/random" alt="" />
+
+                   
+                </div>
+                <div className="thoughts">
+                        <p> Thoughts on your BTech Journey</p>
+                    </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </>
+  )
+}
